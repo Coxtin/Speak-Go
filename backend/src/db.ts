992f50fafb9aeb64836ab90ef480117795ee2557
Coxtin <olteancostin3@gmail.com>
2026-01-25 12:58:07 +0200

    main files and configurations

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,2 +1,30 @@
 import { Pool } from "pg";
 
+
+const pool = new Pool({
+
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: Number(process.env.DB_PORT)
+    
+})
+
+async function verifyConnection(): Promise<void> {
+
+    try{
+
+        const client = pool.connect();
+        console.log('✅ Connected to PostgreSQL database');
+        (await client).release();
+
+    }
+    catch (error){
+        console.error('❌ Error connecting to the database:', error)
+    }
+}
+
+verifyConnection();
+
+export default pool;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,39 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express, {Request, Response} from "express"
 import cors from "cors"
+import pool from "./db";
 
 const port = 5002;
 
+async function getUsers() {
+    const query = 'SELECT * FROM users';
+    try{
+        const data = await pool.query(query);
+        return data.rows;
+    }
+    catch (error){
+        console.error("Eroare :", error);
+        throw error;
+    }
+}
+
 const app = express();
 app.use(cors());
+app.use(express.json()); 
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Salut frate, merge ghine ghine");
-    console.log("salut frate")
+app.get('/', async (req: Request, res: Response) => {
+    try {
+        const users = await getUsers();
+        return res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch users' });
+    }
 });
+
+//app.get('/')
 
 app.listen(port, () => {
     console.log("Server ul merge pe portul : ", port);
 })
-
